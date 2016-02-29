49a311fbf50f08e5b4639a68fdd8e8ee832d78b4
roylee0704 <roylee0704@gmail.com>
2016-02-29 18:44:56 +0800

    removed , so that machines can access via docker

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 // Command-line flags.
 var (
-	httpAddr   = flag.String("http", "localhost:8080", "Listen address")
+	httpAddr   = flag.String("http", ":8080", "Listen address")
 	pollPeriod = flag.Duration("poll", 5*time.Second, "Poll period")
 	version    = flag.String("version", "1.4", "Go version")
 )
